3894e668670790c08408daf261341a6ebfd85ce8
mashdots <josh@mashdots.dev>
2024-06-21 23:27:55 -0700

    fix grid in narrow windows, fix scrolling for lists

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,7 +7,6 @@ const getRandomInt = (size: number): number => {
   const min = Math.ceil(0);
   const max = Math.floor(size);
 
-  // return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   return (Math.floor(Math.pow(10, 14) * Math.random() * Math.random()) % (max - min + 1)) + min;
 };
 
