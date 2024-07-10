d578713a0cda2b34e6c3447e1b19bc7d2037590d
mashdots <josh@mashdots.dev>
2024-07-09 21:18:58 -0700

    expand room name options

---
+++ src/utils/dictionaries/vehicles.ts
@@ -0,0 +1,18 @@
+const vehicles: string[] = [
+  'airplane',
+  'bicycle',
+  'boat',
+  'bus',
+  'car',
+  'helicopter',
+  'motorcycle',
+  'rocket',
+  'scooter',
+  'submarine',
+  'train',
+  'truck',
+  'unicycle',
+  'van',
+];
+
+export default vehicles;
