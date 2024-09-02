2bb2d96750e4df65821bed0525079849ecbcdfea
mashdots <josh@mashdots.dev>
2024-09-01 21:41:36 -0700

    also removing old title control from here

--- src/modules/room/panels/index.ts
+++ src/modules/room/panels/index.ts
@@ -2,7 +2,6 @@ import { DistributionPanel } from './distribution';
 import MultiPanel from './multiPanel';
 import Tickets from './tickets';
 import TimerPanel from './timerPanel';
-import { TitleControl } from './titleControl';
 import VoteDisplay from './voteDisplay';
 import VoteResults from './voteResults';
 import VotingPanel from './votingPanel';
@@ -12,7 +11,6 @@ export {
   MultiPanel,
   Tickets,
   TimerPanel,
-  TitleControl,
   VoteDisplay,
   VotingPanel,
   VoteResults,
