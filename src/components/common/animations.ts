dbe627bf2440e2b4d504a644dd49cd74af3dfa23
mashdots <josh@mashdots.dev>
2024-09-01 21:28:15 -0700

    animations update

--- src/components/common/animations.ts
+++ src/components/common/animations.ts
@@ -13,10 +13,22 @@ export const fadeDownEntrance = keyframes`
   0% {
     opacity: 0;
     transform: translateY(-1rem);
+    filter: blur(0.5rem);
   }
   100% {
     opacity: 1;
     transform: translateY(0);
+    filter: blur(0rem);
   }
 `;
 
+export const scaleEntrance = keyframes`
+  0% {
+    opacity: 0;
+    transform: scale(0);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
