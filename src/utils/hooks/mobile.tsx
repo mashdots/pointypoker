f4cb96a85a6b262090cfee1b6d73f1974e1b6377
mashdots <josh@mashdots.dev>
2024-09-29 13:50:24 -0700

    convert mobile to hook instead of context

--- src/utils/hooks/mobile.tsx
+++ src/utils/hooks/mobile.tsx
@@ -1,50 +1,15 @@
-import React, { useContext } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 type MobileProviderContext = {
-  isMobile: boolean,
-  isMobilePortrait: boolean,
-  isTabletOrMobileDevice: boolean,
-};
-
-type ProviderProps = {
-  children: JSX.Element,
-}
-
-const defaultContext = {
-  isMobile: false,
-  isMobilePortrait: false,
-  isTabletOrMobileDevice: false,
-};
-
-const MobileContext = React.createContext<MobileProviderContext>(defaultContext);
-
-const MobileProvider = ({ children }: ProviderProps): JSX.Element => {
-  const isMobile = useMediaQuery({ maxWidth: 812 });
-  const isMobilePortrait = useMediaQuery({ maxWidth: '812px', orientation: 'portrait' });
-  const isTabletOrMobileDevice = useMediaQuery({ query: '(max-device-width: 75em)' });
-
-  return (
-    <MobileContext.Provider
-      value={{
-        isMobile,
-        isMobilePortrait,
-        isTabletOrMobileDevice,
-      }}
-    >
-      {children}
-    </MobileContext.Provider>
-  );
+  isNarrow: boolean,
 };
 
 const useMobile = (): MobileProviderContext => {
-  const mobile = useContext(MobileContext);
+  const isNarrow = useMediaQuery({ maxWidth: 812 });
 
-  if (mobile === null) {
-    throw new Error('useMobile() called outside of a MobileProvider');
-  }
-
-  return mobile;
+  return {
+    isNarrow,
+  };
 };
 
-export { MobileContext, MobileProvider, useMobile };
+export { useMobile };
