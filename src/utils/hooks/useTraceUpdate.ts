0f3b41125961387c26418a3f48c60998caac9eb1
mashdots <josh@mashdots.dev>
2026-01-26 00:31:19 -0800

    add usetraceupdate hook

---
+++ src/utils/hooks/useTraceUpdate.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+function useTraceUpdate(props: Record<string, unknown>) {
+  const prev = useRef(props);
+  useEffect(() => {
+    const changedProps = Object
+      .entries(props)
+      .reduce((ps: Record<string, [unknown, unknown]>, [k, v]) => {
+        if (prev.current[k] !== v) {
+          ps[k] = [prev.current[k], v];
+        }
+        return ps;
+      }, {});
+
+    if (Object.keys(changedProps).length > 0) {
+      console.log('Changed props:', changedProps);
+    }
+
+    prev.current = props;
+  });
+}
+
+export default useTraceUpdate;
